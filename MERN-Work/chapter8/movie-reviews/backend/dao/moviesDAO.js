7ea36c828fff61c2597318b709640411623fceb6
precious <bhembepreciouspt@gmail.com>
2024-06-07 06:19:59 +0200

    MERN

--- MERN-Work/chapter8/movie-reviews/backend/dao/moviesDAO.js
+++ MERN-Work/chapter8/movie-reviews/backend/dao/moviesDAO.js
@@ -24,10 +24,12 @@ export default class MoviesDAO {
         query = { rated: { $eq: filters["rated"] } };
       }
     }
+
     let cursor;
     try {
       cursor = await movies
         .find(query)
+
         .limit(moviesPerPage)
         .skip(moviesPerPage * page);
       const moviesList = await cursor.toArray();
--- MERN-Work/chapter8/movie-reviews/backend/server.js
+++ MERN-Work/chapter8/movie-reviews/backend/server.js
@@ -9,3 +9,4 @@ app.use("*", (req, res) => {
   res.status(404).json({ error: "not found" });
 });
 export default app;
+ 
