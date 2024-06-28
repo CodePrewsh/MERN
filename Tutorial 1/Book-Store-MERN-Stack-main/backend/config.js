0de46b0b16b964c8b9dbee48fc346d91dc09623a
precious <bhembepreciouspt@gmail.com>
2024-06-28 14:12:49 +0200

    MERN

---
+++ Tutorial 1/Book-Store-MERN-Stack-main/backend/config.js
@@ -0,0 +1,6 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+'mongodb+srv://bhembepreciouspt:123456precious@mern.5axes9f.mongodb.net/?retryWrites=true&w=majority&appName=MERN'
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
