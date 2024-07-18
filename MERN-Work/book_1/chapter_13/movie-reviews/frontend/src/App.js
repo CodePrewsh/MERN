ae5d3002cac6d34e349c34f90b573678f10a9924
precious <bhembepreciouspt@gmail.com>
2024-07-18 22:51:00 +0200

    updates

---
+++ MERN-Work/book_1/chapter_13/movie-reviews/frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div>This is my app</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ Tutorial 1/Book-Store-MERN-Stack-main/backend/config.js
@@ -0,0 +1,8 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://bhembepreciouspt:thandeka123@mern.5axes9f.mongodb.net/?retryWrites=true&w=majority&appName=MERN';
+
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
